5f627445db374b52a21fdb8e7faada5a14d744da
yujoengin <yujoengin123@gmail.com>
2017-03-08 16:23:06 +0900

    complete write button

--- process.php
+++ process.php
@@ -3,17 +3,22 @@
   $conn = mysqli_connect('localhost', 'root', 'yujoengin');
   mysqli_select_db($conn, 'opentutorials2');
   //작성자가 user테이블에 존재하는지 여부를 체크
-  $sql = "SELECT * FROM `user` WHERE `name` = '".mysqli_real_escape_string($conn,$_POST['name'])."'";
+  $author=mysqli_real_escape_string($conn, $_POST['name']);
+  $sql = "SELECT * FROM `user` WHERE `name` = '{$author}';";
   $result = mysqli_query($conn,$sql);
-  var_dump($result);
   if($result->num_rows > 0) {//존재한다면 user.id를 알아낸다.
     $row=mysqli_fetch_assoc($result);
-    var_dump($row);
+    $user_id=$row['id'];
   }
   else {//존재하지 않다면 저자를 user 추가 후 id를 알아낸다.
-
+    $sql = "INSERT INTO user (`id`, `name`) VALUES(NULL, '{$author}')";
+    $result = mysqli_query($conn, $sql);
+    $user_id = mysqli_insert_id($conn);
   }
-
-
   //제목, 저자, 본문 등을 topic 테이블에 추가
+  $title = mysqli_real_escape_string($conn, $_POST['title']);
+  $description = mysqli_real_escape_string($conn, $_POST['description']);
+  $sql = "INSERT INTO `topic` (`id`,`title`,`description`,`author`,`created`)
+          VALUES (NULL,'{$title}','{$description}','{$user_id}',now())";
+  mysqli_query($conn, $sql);
 ?>
